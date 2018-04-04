db9db6df1821e56aa18b9dd67abb711cf7232d4a
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-04 22:25:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+logShout.toUpperCase(string)
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+logWhisper.toLowerCase(string)
+
+function sayHiToGrandma(string){
+  if(string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+else (string === string.toUpperCase()){
+  return "YES INDEED!"
+}
+function sayHiToGrandma(string){
+  if (string === console.log("I love you, Grandma.")) {
+    return "I love you, too."
+  }
+}
