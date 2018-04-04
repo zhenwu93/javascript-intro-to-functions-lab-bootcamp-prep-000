2808fe6602a2ea27e65947ea7768081c3ec51283
Zhen Wei Wu <zhenwu93@gmail.com>
2018-04-04 22:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string === string.toLowerCase()){
     return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
   }
-else (string === string.toUpperCase()){
-  return "YES INDEED!"
 }
-
